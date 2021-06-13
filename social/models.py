67bcc21a52057aa11c84b77a993efb1ab8d52464
ST Liar <bohdansapovsky@gmail.com>
2021-06-13 21:52:04 +0300

    Updated models

--- social/models.py
+++ social/models.py
@@ -2,19 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class BaseDateModel(model.Model):
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+
 class User(AbstractBaseUser):
     avatar = models.ImageField(upload_to="avatars/")
     banner = models.ImageField(upload_to="banners/")
 
-    subscriptions = models.ManyToManyField('self')
+    subscriptions = models.ManyToManyField('self', symmetrical=False)
 
-class SubscriptionRequestModel(models.Model):
+class SubscriptionRequestModel(BaseDateModel)):
     author = models.ForeignKey(User, related_name="+")
-    target = models.ForeignKey(Userm related_name="subscription_requests")
-    date = models.DateField(auto_now_add=True) 
+    target = models.ForeignKey(Userm related_name="subscription_requests") 
 
-class SystemMessageModel(models.Model):
-    date = models.DateField(auto_now_add=True)
+class SystemMessageModel(BaseDateModel):
     text = models.CharField(max_length=120, null=False)
     read = models.BooleanField(default=False)
     target = models.ForeignKey(User, related_name="system_messages")
