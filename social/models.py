b61a75bc4d32a96f8abefefed0f9336e879f0516
ST Liar <bohdansapovsky@gmail.com>
2021-06-14 20:39:45 +0300

    refaktor social  models

--- social/models.py
+++ social/models.py
@@ -6,17 +6,21 @@ class BaseDateModel(model.Model):
     created = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now=True)
 
-class User(AbstractBaseUser):
+class BaseTargetModel(BaseDateModel):
+    target_related_name = "target_set"
+    target = models.ForeignKey(UserModel, related_name=target_related_name) 
+
+class UserModel(AbstractBaseUser):
     avatar = models.ImageField(upload_to="avatars/")
     banner = models.ImageField(upload_to="banners/")
 
     subscriptions = models.ManyToManyField('self', symmetrical=False)
 
-class SubscriptionRequestModel(BaseDateModel):
-    author = models.ForeignKey(User, related_name="+")
-    target = models.ForeignKey(User, related_name="subscription_requests") 
+class SubscriptionRequestModel(BaseTargetModel):
+    target_related_name="subscription_requests"
+    author = models.ForeignKey(UserModel, related_name="+") 
 
-class SystemMessageModel(BaseDateModel):
+class SystemMessageModel(BaseTargetModel):
     text = models.CharField(max_length=120, null=False)
     read = models.BooleanField(default=False)
-    target = models.ForeignKey(User, related_name="system_messages")
+    target_related_name="system_messages"
