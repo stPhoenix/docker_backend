5a998f38114a49c93e97414a7686c0badcc0b495
ST Liar <bohdansapovsky@gmail.com>
2021-06-15 22:41:26 +0300

    add auth

--- backend/settings.py
+++ backend/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
